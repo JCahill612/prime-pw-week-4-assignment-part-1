1ec924d47bc45c7955ebcbf7babd5fa6c29cf9da
jcahill612 <jenny.e.cahill@gmail.com>
2021-08-22 21:15:48 -0500

    Completed Week 4 Part 1 Assignment

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -107,16 +107,16 @@ console.log('The animal lizard is in colors', (find('lizard',colors)));
 // ----------------------
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
-function isFirstLetter() {
-  let fruit = ('a', 'apple');
- // let fruit2 =  fruit.charAt(0);
- // console.log( fruit2 );
-  if(fruit.charAt(0) == ('a')){
-    return true;
+let fruit = ['a', 'apple'];
+
+function isFirstLetter(letter, fruit) {
+  for( let i=0; i < fruit.length; i++ ) {
+    fruit.charAt(i);
+    if(fruit.charAt[i]===[]){
+      return true;
+    }
   }
-  else{
-    return false;
-  }
+  return false;
 }
 
 
@@ -125,16 +125,29 @@ console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
-  // TODO: loop to add items
-  return sum;
+// 9. Function to return the sum of all numbers in an array
+let cars = [];
+function sumAll(cars) {
+  let max = Math.max(cars[0], cars[1]);
+  let min = Math.min(cars[0], cars[1]);
+  let sumBetween = 0;
+  for (let i = min; i <= max; i++) {
+    sumBetween += i;
+  }
+  return sumBetween;
 }
+console.log('The sum is:', sumAll([1, 4]));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
 
-
+let numArr = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
+function getPositives(ar){
+  const posArr = ar.filter(num => num > -1);
+  return posArr;
+}
+console.log('Positive numbers are:', getPositives([-5,10]));
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
